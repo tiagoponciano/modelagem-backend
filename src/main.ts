ba913bdaed2cc7733207c7145c333568c58a31ac
tiagoponciano <tiagoponciano03@gmail.com>
2025-12-08 22:55:52 -0300

    chore: update Swagger setup route from 'api' to 'docs' for improved documentation access

--- src/main.ts
+++ src/main.ts
@@ -33,7 +33,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('docs', app, document);
 
   await app.init();
   cachedApp = expressApp;
